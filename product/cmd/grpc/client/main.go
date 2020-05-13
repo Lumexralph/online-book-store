516f450a2a0a7a0c1acf98a02f792ef1be7f1f73
Olumide Ogundele <olumideralph@gmail.com>
2020-05-13 13:24:14 +0100

    create client

--- product/cmd/grpc/client/main.go
+++ product/cmd/grpc/client/main.go
@@ -1,2 +1,49 @@
 // Package main contains the implementation of the grpc client
 package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	domain "product/internal/grpc/domain_service"
+
+	"github.com/golang/protobuf/ptypes"
+
+	"google.golang.org/grpc"
+)
+
+const address = ":5000"
+
+func main() {
+	// make a grpc server connection
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	client := domain.NewProductServiceClient(conn)
+
+	product := &domain.AddProductRequest{
+		Product: &domain.Product{
+			Id:          "1",
+			Name:        "Sherlock Holmes",
+			Description: "It is an investigative book",
+			Price:       123.56,
+			Slug:        "sherlock-holmes",
+			Active:      true,
+			InStock:     true,
+			DateUpdated: ptypes.TimestampNow(),
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	//	try creating a product
+	resp, err := client.AddProduct(ctx, product)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("response: ", resp)
+}
